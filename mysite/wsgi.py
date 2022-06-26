f93df3fd6387968b08fb03db20fb9e765b5aa275
bobbyzto <bobbyweche@gmail.com>
2022-06-26 11:19:46 -0400

    wsgi edit

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -6,10 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+
+project_folder = os.path.expanduser('~/bobbyzto/Desktop/basic-site/')  # adjust as appropriate thats my directory
+load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
